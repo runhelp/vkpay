4d9100f87bf11662cb223eb5f8fdf72af787af0c
runhelp <runhelp@mail.ru>
2019-03-23 15:09:01 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Panel, PanelHeader, Copyrights, Title, ListItem, Avatar } from '@vkontakte/vkui';
+import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Антивирусное ПО компании G Data</PanelHeader>
+		<PanelHeader>Example</PanelHeader>
 		{fetchedUser &&
-		<Group title="Пользователькие данные">
+		<Group title="User Data Fetched with VK Connect">
 			<ListItem
 				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
 				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
@@ -14,17 +14,12 @@ const Home = ({ id, go, fetchedUser }) => (
 				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
 			</ListItem>
 		</Group>}
-		<Group title="Приложение в разработке, дождитесь пока мы работаем!">
+
+		<Group title="Navigation Example">
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Пожалуйста покажите мне Персика
+					Show me the Persik, please
 				</Button>
-			</Div>
-		</Group>
-
-		<Group copyrights="Группа компаний Бегущая Помощь">
-			<Div>
-					Антивирусное программное обеспечение от группы компаний Бегущая Помощь, работаем по всей России.
 			</Div>
 		</Group>
 	</Panel>
