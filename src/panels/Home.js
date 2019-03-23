b837373bbbbb456707c303a10c57f749f5b1f935
runhelp <runhelp@mail.ru>
2019-03-23 15:02:18 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -4,11 +4,7 @@ import { Panel, PanelHeader, Copyrights } from '@vkontakte/vkui';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-<<<<<<< HEAD
-		<PanelHeader>Антивирусное ПО компании G </PanelHeader>
-=======
 		<PanelHeader>Антивирусное ПО компании G Data</PanelHeader>
->>>>>>> 091860a007c08bc2e20e78ea61e181ded1878902
 		{fetchedUser &&
 		<Group title="Пользователькие данные">
 			<ListItem
@@ -18,12 +14,7 @@ const Home = ({ id, go, fetchedUser }) => (
 				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
 			</ListItem>
 		</Group>}
-
-<<<<<<< HEAD
-		<Group title="Приложение в разработке, дождитесь пока мы работаем">
-=======
 		<Group title="Приложение в разработке, дождитесь пока мы работаем!">
->>>>>>> 091860a007c08bc2e20e78ea61e181ded1878902
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
 					Пожалуйста покажите мне Персика
