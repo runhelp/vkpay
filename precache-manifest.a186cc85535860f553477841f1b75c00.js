0b0ea9473be167422561a511873ebd83d9c9b84c
runhelp <runhelp@mail.ru>
2019-03-23 15:39:30 +0300

    Updates

---
+++ precache-manifest.a186cc85535860f553477841f1b75c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "//github.com/runhelp/vkpay/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "4236daf3410fd7ab7073",
+    "url": "//github.com/runhelp/vkpay/static/js/runtime~main.4236daf3.js"
+  },
+  {
+    "revision": "7917899331127e0209e0",
+    "url": "//github.com/runhelp/vkpay/static/js/main.79178993.chunk.js"
+  },
+  {
+    "revision": "48e4160ed52a8ac4cf96",
+    "url": "//github.com/runhelp/vkpay/static/js/2.48e4160e.chunk.js"
+  },
+  {
+    "revision": "7917899331127e0209e0",
+    "url": "//github.com/runhelp/vkpay/static/css/main.740b4f5b.chunk.css"
+  },
+  {
+    "revision": "48e4160ed52a8ac4cf96",
+    "url": "//github.com/runhelp/vkpay/static/css/2.0d804938.chunk.css"
+  },
+  {
+    "revision": "af8569bfaeea6522b1e3ae9bbbf5d7c6",
+    "url": "//github.com/runhelp/vkpay/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vkpay/precache-manifest.904e446cde680ecbef4c7084ccff18de.js"
+  "//github.com/runhelp/vkpay/precache-manifest.a186cc85535860f553477841f1b75c00.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vkpay/index.html", {
+workbox.routing.registerNavigationRoute("//github.com/runhelp/vkpay/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
